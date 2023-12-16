4dacbbec47e92647b6012bba140db990028a1c9a
Ismael njihia <engishmaelnjihia@gmail.com>
2023-12-17 00:04:58 +0300

    Added some packages, I need in Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <table className="table-fixed border-2 border-red-900">
+      <thead>
+        <tr>
+          <th className="w-1/4 p-2 font-bold text-center">Column 1</th>
+          <th className="w-1/4 p-2 font-bold text-center">Column 2</th>
+          <th className="w-1/4 p-2 font-bold text-center">Column 3</th>
+          <th className="w-1/4 p-2 font-bold text-center">Column 4</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="p-2 text-center font-bold">Data 1</td>
+          <td className="p-2 text-center font-bold">Data 2</td>
+          <td className="p-2 text-center font-bold">Data 3</td>
+          <td className="p-2 text-center font-bold">Data 4</td>
+        </tr>
+        <tr>
+          <td className="p-2 text-center font-bold">Data 5</td>
+          <td className="p-2 text-center font-bold">Data 6</td>
+          <td className="p-2 text-center font-bold">Data 7</td>
+          <td className="p-2 text-center font-bold">Data 8</td>
+        </tr>
+        <tr>
+          <td className="p-2 text-center font-bold">Data 9</td>
+          <td className="p-2 text-center font-bold">Data 10</td>
+          <td className="p-2 text-center font-bold">Data 11</td>
+          <td className="p-2 text-center font-bold">Data 12</td>
+        </tr>
+        <tr>
+          <td className="p-2 text-center font-bold">Data 13</td>
+          <td className="p-2 text-center font-bold">Data 14</td>
+          <td className="p-2 text-center font-bold">Data 15</td>
+          <td className="p-2 text-center font-bold">Data 16</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  prefix: 'tw-', // default: 'tw-'
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+}
+
+
+
