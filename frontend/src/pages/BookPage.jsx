28f2a83da3b817a77784bb16ebccda3a27aacad4
Ismael njihia <engishmaelnjihia@gmail.com>
2023-12-19 05:58:04 +0300

    Worked on the frontend - The Landing Page that is

--- frontend/src/pages/BookPage.jsx
+++ frontend/src/pages/BookPage.jsx
@@ -1,18 +1,18 @@
 import Layout from '../components/Layout';
 import Header from '../components/Header';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 
-const BookPage = () => {
+
+const BookPage = (props) => {
     //get id in url
-   
-
-   const { id } = useParams();
+    const {id} = useParams();
+    const decodedId = decodeURIComponent(id);
   return (
     <>
     <Header />
     <Layout>
         <h1>
-            Book Page {id}
+            Book Page {decodedId}
         </h1>
     </Layout>
     </>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 import Layout from "../components/Layout"
 import Header from "../components/Header"
 import {useFetchBooksQuery} from '../slices/BooksApiSlice';
-
+import { Row, Col } from "react-bootstrap";
+import '../assets/HomePage.css'
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
 
@@ -18,21 +20,44 @@ const Homepage = () => {
         ) : error ? (
           <h3>{error}</h3>
         ) : (
-          books.map((book) => (
-            <div key={book.book_id} className='bookCard'>
-              <div className='bookImage'>
-                <img src={book.image_url} alt={book.book_name} />
+          <Row xs={1} md={2} lg={4}>
+  {books &&
+    books.map((book) => {
+      const shortDescription = book.description_.substring(0, 100);
+      const available_copies = book.available_copies;
+      const book_id = book.book_id;
+      const encodedId = encodeURIComponent(book_id);
+
+      const isNotAvailable = available_copies <= 0;
+
+      return (
+        <Col key={book.book_id}>
+          <Link to={`/book/${encodedId}`}>
+            <div className="bookCard" style={{marginBottom:"10px", position:"relative"}}>
+              {isNotAvailable && (
+                <div className="overlayEmpty">
+                  <p>Not Available</p>
+                </div>
+              )}
+              <div className="bookCard__image" style={{height: "160px", overflow: "hidden"}}>
+                <img
+                  src={book.image}
+                  alt={book.title}
+                  style={{width: "100%", height:"100%", objectFit:"cover", objectPosition:"top"}}/>
+                </div>
+
+
+                <div className="bookCard__body">
+                  <p className="book_title"> {book.title}</p>
+                  <p className="shortDescription">{shortDescription}...</p>
+                </div>  
               </div>
-              <div className='bookInfo'>
-                <h3>{book.title}</h3>
-                <p>{book.author}</p>
-                <p>{book.published_date}</p>
-                <p>{book.genre_id}</p>
-                <p>{book.description_}</p>
-                <p>{book.total_copies}</p>
-              </div>
-            </div>
-          ))
+            </Link>
+        </Col>
+      );
+    })}
+</Row>
+
         )}
        </div>
     </Layout>
